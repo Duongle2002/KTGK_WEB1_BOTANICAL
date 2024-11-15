ee6524641ff30578358e3299461fd20e7d0aeba0
Duong <duong100008@donga.edu.vn>
2024-11-15 11:09:13 +0700

    fix some

--- src/main/java/com/example/KTCK/controller/HomeController.java
+++ src/main/java/com/example/KTCK/controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.example.KTCK.controller;
 
+import com.example.KTCK.model.Products;
+import com.example.KTCK.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
-    
+
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
     public String homePage(){
         return "home";
     }
     @GetMapping("/home2")
-    public String home2Page(){
+    public String home2Page(@RequestParam(defaultValue = "0") int page, Model model){
+        int size = 4; // Number of products per page
+        Page<Products> productPage = productService.findAll(PageRequest.of(page, size));
+        model.addAttribute("productPage", productPage.getContent());
         return "home2";
     }
 
